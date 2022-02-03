4625aabb7ea83b40ff462e786192be0574c27f01
Caio <caiopadilha@id.uff.br>
2022-02-03 16:19:52 -0300

    Requisito 1 teste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+
+        <Route path="/" Component={ Login } />
+        <Route path="/search" Component={ Search } />
+        <Route path="/album/:id" Component={ Album } />
+        <Route path="/favorites" Component={ Favorites } />
+        <Route path="/profile/edit" Component={ ProfileEdit } />
+        <Route path="/profile" Component={ Profile } />
+        <Route path="/*" Component={ NotFound } />
+
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <span>Page not found</span>
+      </div>
+    );
+  }
+}
+
+export default NotFound;

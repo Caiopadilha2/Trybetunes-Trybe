606e16a98015f2af701bde480877e9258f4861b7
Caio <caiopadilha@id.uff.br>
2022-02-03 19:16:57 -0300

    Requisito 4 ok completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
-      <header data-testid="header-component">Header</header>
+      <div>
+        <nav>
+
+          <Link to="/search" data-testid="link-to-search">Procurar</Link>
+
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+
+          <Link to="/profile" data-testid="link-to-profile"> Meu Perfil</Link>
+
+        </nav>
+      </div>
     );
   }
 }
+// Aula dia 12.2 minuto 00:23 até 00:26 do Cestari.
 
 export default Header;
